e08d7ed7d57e520daf7ea8b86419fc7d47711125
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-08 00:03:46 +0530

    [aditya]: create a simple linked list

---
+++ LinkedList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.Next!= null)
+                {
+                    temp = temp.Next;   
+                }
+                temp.Next=node;
+            }
+            Console.WriteLine("{0} is inserted into the lined list", +data);
+
+        }
+       
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           LinkedList<int> list = new LinkedList<int>();
+            list.AddFirst(56);
+            list.AddLast(30);
+            list.AddLast(70);
+
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
